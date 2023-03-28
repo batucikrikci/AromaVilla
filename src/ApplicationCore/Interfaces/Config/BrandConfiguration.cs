02028dc69f7206ce051c25e1787b8826ee954f59
batucikrikci <batuhanncikrikci@gmail.com>
2023-03-28 12:27:12 +0300

    entities, interface, dbcontext

---
+++ src/ApplicationCore/Interfaces/Config/BrandConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationCore.Interfaces.Config
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(x => x.Name)
+                .HasMaxLength(100);
+        }
+
+      
+    }
+}
